7b564a01f3ab97dd9c80ae5e3594890add7c10ce
nhyun199 <shgus1692@gmail.com>
2024-01-29 13:42:26 +0900

    Folder Renamed and Add Build File
    - Folder renamed - Before : 2048 project / After : Copied_2048
    - Add build file.

---
+++ Copied_2048/Assets/1. Scripts/ButtonClickEvent/TitleButton.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleButton : MonoBehaviour
+{
+    public Canvas BestScoreCanvas;
+    public Canvas ResetCanvas;
+
+    public void Button_Start()
+    {
+        Debug.Log("GAME START!");
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void Button_Exit()
+    {
+        Debug.Log("게임을 종료합니다!");
+        // 에디터에서 동작할 때
+        //UnityEditor.EditorApplication.isPlaying = false;
+        // 빌드했을 때
+        Application.Quit();
+    }
+
+    public void Button_BestScore()
+    {
+        BestScoreCanvas.enabled = true;
+    }
+
+    public void Button_BestScoreExit()
+    {
+        BestScoreCanvas.enabled = false;
+    }
+
+    public void Button_Reset()
+    {
+        ResetCanvas.enabled = true;
+    }
+
+    public void Button_Reset_No()
+    {
+        ResetCanvas.enabled = false;
+    }
+}
---
+++ Copied_2048/Assets/1. Scripts/Score/ScoreRecord.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Puzzle_2048
+{
+    [Serializable]
+    public class ScoreRecord
+    {
+        public int score;
+        public string initials;
+    }
+}
